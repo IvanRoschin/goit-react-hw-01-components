0ed17f55bba05619168b13ee5342c00f47fce0e9
Ivan Roschin <ivan.roschin86@gmail.com>
2022-10-24 21:56:14 +0300

    update profile styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
 import { App } from 'components/App/App.jsx';
-import { theme } from 'theme/index.jsx';
+import { theme } from 'theme/theme';
 
 console.log(theme);
 
