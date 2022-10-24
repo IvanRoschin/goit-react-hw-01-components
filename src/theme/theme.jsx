fa6295578160a8671ca431988b5a4404c80c9723
Ivan Roschin <ivan.roschin86@gmail.com>
2022-10-24 22:01:52 +0300

    update profile styles

---
+++ src/theme/theme.jsx
@@ -0,0 +1,50 @@
+export const theme = Object.freeze({
+  colors: {
+    black: '#000',
+    white: '#fff',
+    text: '#2',
+    background: '#fff',
+    primary: '#07c',
+    secondary: '#05a',
+    accent: '#609',
+    muted: '#f6f6f6',
+    graytext: '#798998',
+    gratbackprim: '#f3f6f9',
+    gratbacksec: '#e7ecf2',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '32px',
+    xl: '64px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+    round: '50%',
+  },
+  shadowbox: {
+    profile: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;',
+  },
+});
